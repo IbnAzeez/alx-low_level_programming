16ecf70eaf790cfadc32664ab3cb0983451ee1a9
IbnAzeez <hayzed003@gmail.com>
2022-09-08 15:56:55 +0000

    Put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
 #!/bin/bash
-cat programming
+#include<stdio.h>
+int main()
+{
+    char Mystr[] = "Programming is like building a multilingual puzzle";
+    
+    puts(Mystr); 
+    
+    return 0;
+}
