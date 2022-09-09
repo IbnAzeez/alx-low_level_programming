14825deba5fd0e32820130a57713a84bcdf693a4
IbnAzeez <hayzed003@gmail.com>
2022-09-09 21:51:52 +0000

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x);
+	}
+	return (0);
+}
