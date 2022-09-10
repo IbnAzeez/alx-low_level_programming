930515fbb0ba36593ae7ad2e50e2ea58b995c07c
IbnAzeez <hayzed003@gmail.com>
2022-09-10 00:22:36 +0000

    Print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '48'; x <= '58'; x++)
+	{
+		putchar(x);
+	}
+	for (y = 'A'; y <= 'F'; y++)
+	{
+		putchar(tolower(y));
+	}
+	putchar('\n');
+	return (0);
+}
