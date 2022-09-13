d01df43f3b9a56db0be60180df05d3504c8a14a6
IbnAzeez <hayzed003@gmail.com>
2022-09-13 14:50:52 -0700

    Alphabets

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,6 +3,7 @@
 
 /**
  * print_sign - prints the sign of a number.
+ * @n: the int to check
  * Returns 1 and prints + if n is greater than zero
  * Returns 0 and prints 0 if n is zero
  * Returns -1 and prints - if n is less than zero
