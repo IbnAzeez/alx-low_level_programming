149ad25051bae8f0eb8807871708ab9ad6113d8b
IbnAzeez <hayzed003@gmail.com>
2022-09-16 22:40:05 -0700

    Print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,7 +9,8 @@ void print_diagonal(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-	} else
+	} 
+	else
 	{
 		int i, j;
 
@@ -23,9 +24,11 @@ void print_diagonal(int n)
 				}
 				else if
 				{
-					(j < i)
+					(j < i);
 					_putchar(' ');
 					_putchar('\n');
 				}
 			}
+		}
+	}
 }
