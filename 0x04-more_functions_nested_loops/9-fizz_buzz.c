23c72b3529f977a84c955989af87eac6f8b22b54
IbnAzeez <hayzed003@gmail.com>
2022-09-25 06:58:40 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,26 +17,17 @@ int main(void)
 		{
 			printf(" Fizz");
 		} 
-		else 
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			if (i % 5 == 0 && i % 3 != 0)
-			{
-				printf(" Buzz");
-			}
+			printf(" Buzz");
 		}	
-		else
-		{	
-			if (i % 3 == 0 && i % 5 == 0)
-			{
-				printf(" FizzBuzz");
-			}
-		}	
-		else 
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			if (i == 1)
-			{
-				printf("%d", i);
-			}
+			printf(" FizzBuzz");
+		}	
+		else if (i == 1)
+		{
+			printf("%d", i);
 		}	
 		else
 		{
