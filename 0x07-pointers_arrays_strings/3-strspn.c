ca8515695d71f3c1bf37eb01ecfbf02b06877db3
IbnAzeez <hayzed003@gmail.com>
2022-09-26 20:17:52 -0700

    length

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, c, matchh;
+
+	c = 0;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		match = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				c++;
+				match = 1;
+			}
+		}
+		if (match == 0)
+		{
+			return (c);
+		}
+		i++;
+	}
+	return (0);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,3 +3,5 @@
 1-memcpy.c copies memory area.
 
 2-strchr.c locates a character in a string.
+
+3-strspn.c gets the length of a prefix substring.
