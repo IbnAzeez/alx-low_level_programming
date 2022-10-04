d23810c4af0121e8b17d4d327147394eae0f8511
IbnAzeez <hayzed003@gmail.com>
2022-10-04 03:11:23 -0700

    Multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,24 +18,25 @@ int _atoi(char *s)
 
 	for (length = 0; s[length] != '\0'; length++)
 	{
+
 	}
-		while (i < length && x == 0)
+	while (i < length && x == 0)
+	{
+		if (s[i] == '-')
+			++y;
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (s[i] == '-')
-				++y;
-			if (s[i] >= '0' && s[i] <= '9')
-			{
-				num = s[i] - '0';
-				if (y % 2)		
-					num = -num;		
-				n = n * 10 + num;
-				x = 1;
-				if (s[i + 1] < '0' || s[i + 1] > '9')
-					break;
-				x = 0;
-			}
-			i++;
+			num = s[i] - '0';
+			if (y % 2)
+				num = -num;
+			n = n * 10 + num;
+			x = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			x = 0;
 		}
+		i++;
+	}
 	if (x == 0)
 		return (0);
 	return (n);
@@ -50,13 +51,13 @@ int _atoi(char *s)
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
-	
+
 	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
+
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
