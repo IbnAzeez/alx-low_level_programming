4606239293c686b4b7fa320fff96ec5d6228f7a3
IbnAzeez <hayzed003@gmail.com>
2022-10-06 15:20:46 -0700

    calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef FILE_H
 #define FILE_H
 
+char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
