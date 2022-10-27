255e1f335250bcf84a80cc60d1d0731f52eda9ea
IbnAzeez <hayzed003@gmail.com>
2022-10-27 16:04:01 -0700

    Print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,5 +10,5 @@ void print_binary(unsigned long int n)
 	unsigned int i;
 
 	for (i = 1 << 31; i > 0; i = i / 2)
-		(n & i) ? _putchar("1") : _putchar("0");
+		(n & i) ? _putchar('1') : _putchar('0');
 }
