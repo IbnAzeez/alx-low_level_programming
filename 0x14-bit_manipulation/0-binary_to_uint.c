48350b827891853540fc2756a5d8903a08eb9590
IbnAzeez <hayzed003@gmail.com>
2022-10-27 14:34:23 -0700

    Initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: points to a string of 0 and 1
+ * Return: the converted number, or 0 if 
+ * there is one or more chars in the string b that is not 0 or 1 
+ * or b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int l;
+	unsigned int x;
+	unsigned int y;
+	int i;
+       
+	x = 1;
+	y = 0;
+	l = _strlen(b);
+	i = l - 1;
+
+	while (i >= 0)
+/*	for (c = len-1; c >= 0; c--)*/
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] == '1')
+		{
+			y += x;
+		}
+		x *= 2;
+	}
+
+	return (y);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef FUNC
+#define FUNC
+
+int _strlen(char *s);
+unsigned int binary_to_uint(const char *b);
+
+#endif
