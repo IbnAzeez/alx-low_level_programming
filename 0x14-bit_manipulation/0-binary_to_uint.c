8d9705efd1f3cbe54c8725e7eb28e91ca15c0beb
IbnAzeez <hayzed003@gmail.com>
2022-10-27 15:28:22 -0700

    Binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,7 +20,11 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 
 	while (b[i] != '\0')
-/*	for (c = len-1; c >= 0; c--)*/
+		i++;
+
+	i = i - 1;
+
+	while (i >= 0)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
