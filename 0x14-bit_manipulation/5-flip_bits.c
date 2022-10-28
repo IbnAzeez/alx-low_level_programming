c05766da3bd5a98dd2ec008260029280943c4188
IbnAzeez <hayzed003@gmail.com>
2022-10-27 18:49:11 -0700

    Flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,7 +4,7 @@
  * flip_bits - Sets the value of a bit at a given index to 1.
  * @n: A pointer to the bit.
  * @m: The index to set the value at - indices start at 0
- * Return: number of bits you needed to flip to get from one number to another. 
+ * Return: number of bits you needed to flip to get from one number to another.
  */
 
 /**
@@ -18,15 +18,14 @@ int count(int n)
 
 	i = 0;
 
-	while (n > 0) {
+	while (n > 0)
+       	{
 		i++;
 		n &= (n - 1);
 	}
-	return i;
+	return (i);
 }
-
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return count(n ^ m);
+	return (count(n ^ m));
 }
-
