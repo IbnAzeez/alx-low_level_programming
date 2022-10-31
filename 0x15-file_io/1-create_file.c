299949b6b51e5977bd12944ff632d1bf1b8f7b52
IbnAzeez <hayzed003@gmail.com>
2022-10-31 14:38:20 -0700

    Create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,13 +15,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
 	if (text_content != NULL)
 	{
 		while (text_content[i])
 			i++;
 	}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, i);
 
 	if (op == -1 || wr == -1)
