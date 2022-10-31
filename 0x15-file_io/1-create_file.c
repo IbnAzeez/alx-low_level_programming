51248d93b2c9f27a7140bb5b6a0dcdeaab97985b
IbnAzeez <hayzed003@gmail.com>
2022-10-31 14:22:34 -0700

    Create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int op;
 	int wr; 
-	int i = 0;
+	int i;
 
 	if (filename == NULL)
 		return (-1);
@@ -18,9 +18,12 @@ int create_file(const char *filename, char *text_content)
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (text_content != NULL)
+	{
+		i = 0;
+
 		while (text_content[i])
 			i++;
-
+	}
 	wr = write(op, text_content, i);
 
 	if (op == -1 || wr == -1)
