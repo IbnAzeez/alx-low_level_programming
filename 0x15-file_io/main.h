56c3a424a4b31eb24fb96f4f9ae9e02ecf6b21a1
IbnAzeez <hayzed003@gmail.com>
2022-11-02 07:39:56 -0700

    Copy

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN
 #define MAIN
 
-#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
