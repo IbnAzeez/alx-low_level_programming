19aaf1fdb34e1d81ae923f99e39eef023c4de25a
IbnAzeez <hayzed003@gmail.com>
2022-11-02 04:57:28 -0700

    Copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ * Return: A pointer to the newly-allocated buffer.
+ */
+char *create_file(char *file)
+{
+	char *temp;
+
+	temp = malloc(sizeof(char) * 1024);
+
+	if (temp == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (temp);
+}
+
+/**
+ * close - Closes file descriptors.
+ * @fc: The file descriptor to be closed.
+ */
+void close(int fc)
+{
+	int c;
+
+	c = close(fc);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fc %d\n", fc);
+		exit(100);
+	}
+}
+
 /**
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
