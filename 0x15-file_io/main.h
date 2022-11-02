8793b4250b48077de130e6bd61859852503ec75b
IbnAzeez <hayzed003@gmail.com>
2022-11-02 07:42:22 -0700

    Copy

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,21 @@
 #ifndef MAIN
 #define MAIN
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _strlen(char *s);
+char *create_buffer(char *file);
+void close_file(int fd);
+int main(int argc, char *argv[]);
 
 #endif
