a2c3ed44bd2cc24a168c2e4086e06efea3e5c816
IbnAzeez <hayzed003@gmail.com>
2023-03-03 02:11:06 -0800

    Perimeter Island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    ''' Module for island perimeter '''
+""" island module """
 
-    def island_perimeter(grid):
-        ''' Returns the perimeter of the island described in grid '''
-        grid = []
+
+def island_perimeter(grid):
+    """
+       calculate the island perimeter
+       Args:
+           grid: map
+    """
+
+    p = 0
+
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if grid[row][column] == 1:
+                if row == 0 or grid[row - 1][column] == 0:
+                    p += 1
+                if column == 0 or grid[row][column - 1] == 0:
+                    p += 1
+                if row == len(grid) - 1 or grid[row + 1][column] == 0:
+                    p += 1
+                if column == len(grid[row]) - 1 or grid[row][column + 1] == 0:
+                    p += 1
+    return p
